d7108320aa3107cd0c784a75bcf4740b690f0627
Delvia Lanasemba <delvia.lana07@gmail.com>
2022-07-18 15:50:26 +0700

    Membuat fitur untuk authentikasi

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 
+const authentication = require('./api/authentication');
+const Database = require('./conf/Database');
+const AuthenticationService = require('./services/mysql/AuthenticationService');
+const AuthenticationValidator = require('./validator/authentication');
+
 const init = async () => {
+  const database = new Database();
+  const AthenticationService = new AuthenticationService(database);
 
     const server = Hapi.server({
       host: process.env.HOST,
@@ -20,6 +27,17 @@ const init = async () => {
           name: 'Rski Mulud Muchamad',
         }),
       });
+
+      // defines external plugin
+      await server.register([
+        {
+          plugin: authentication,
+          options: {
+            service: AuthenticationService,
+            validator: AuthenticationValidator,
+          },
+        },
+      ]);
       
       await server.start();
       console.log(`Server running at ${server.info.uri}`);
